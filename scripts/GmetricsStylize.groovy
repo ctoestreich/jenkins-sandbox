873629ef2ca9c55bb764277f282dd324ba649a04
Christian Oestreich <acetrike@yahoo.com>
2011-04-30 00:25:11 -0500

    Changing location for the codenarc xml file

--- scripts/GmetricsStylize.groovy
+++ scripts/GmetricsStylize.groovy
@@ -1,8 +1,9 @@
 includeTargets << grailsScript("Init")
 
 target(main: "Add some style to the gmetrics report") {
+    ant.mkdir(dir:'target/gmetrics')
 
-    ant.xslt style: "gmetrics/default.xslt", out: "target/gmetrics/default.html", in: 'target/gmetrics.xml'
+    ant.xslt style: "gmetrics/default.xslt", out: "target/gmetrics/gmetrics.html", in: 'target/gmetrics.xml'
     ant.copy(todir: 'target/gmetrics') {
         fileset(dir: 'gmetrics') {
             include name: 'default.css'
